d18d1c0b6b7201530920374edce942f453f4eb2c
alantothe <rubenamalpartida@gmail.com>
2024-06-27 16:48:48 -0400

    add acceptFriendRequest

--- controllers/friendrequest/FriendRequest.js
+++ controllers/friendrequest/FriendRequest.js
@@ -1,4 +1,5 @@
 import FriendRequest from "../../models/FriendRequest.js";
+import User from "../../models/User.js";
 
 export const sendFriendRequest = async (req, res) => {
   const { requisitionerId, recipientId } = req.body;
@@ -9,6 +10,41 @@ export const sendFriendRequest = async (req, res) => {
       status: "pending",
     });
     res.status(201).json({ message: "Friend request sent", request });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const acceptFriendRequest = async (req, res) => {
+  const { _id } = req.params;
+  try {
+    // Update the status
+    const request = await FriendRequest.findByIdAndUpdate(
+      _id,
+      { status: "accepted" },
+      { new: true }
+    ).exec();
+
+    if (!request) {
+      return res.status(404).json({ message: "Friend request not found" });
+    }
+    // Grab IDs from request
+    const { requisitionerId, recipientId } = request;
+
+    await User.findByIdAndUpdate(requisitionerId, {
+      $push: { friends: recipientId },
+    }).exec();
+    await User.findByIdAndUpdate(recipientId, {
+      $push: { friends: requisitionerId },
+    }).exec();
+
+    res.json({
+      message: "Friend request accepted",
+      request,
+      requisitionerId,
+      recipientId,
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: "Internal server error" });
--- routes/FriendRequest.js
+++ routes/FriendRequest.js
@@ -3,5 +3,6 @@ import * as controllers from "../controllers/friendrequest/FriendRequest.js";
 
 const router = Router();
 router.post("/request", controllers.sendFriendRequest);
+router.patch("/accept/:_id", controllers.acceptFriendRequest);
 
 export default router;
