79afcc79424c2fb107cdcd61fcc17274d98876dd
alantothe <rubenamalpartida@gmail.com>
2024-08-01 13:42:21 -0400

    change get to post

--- routes/User.js
+++ routes/User.js
@@ -7,10 +7,10 @@ const router = Router();
 router.post("/register", auth.register);
 router.post("/login", auth.login);
 router.patch("/reset", auth.resetPassword);
+router.post("/limited", controllers.limitedUser);
 router.patch("/open-chat", controllers.openConversation);
 router.patch("/close-chat", controllers.closeConversation);
 router.patch("/update/:_id", controllers.updateUser);
-router.get("/limited", controllers.limitedUser);
 router.get("/entire/:_id", controllers.entireUser);
 
 export default router;
